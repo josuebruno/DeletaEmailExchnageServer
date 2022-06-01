58fbf5368c19d61bbd5762a150b48b4cf5b9fff8
Josué Soares Bruno <73852814+josuebruno@users.noreply.github.com>
2022-06-01 14:46:18 -0300

    Primeiro commit

---
+++ Scripts/Remov_Emails_Spam.ps1
@@ -0,0 +1,23 @@
+﻿#pega lista de e-mails 
+
+$afetados = Get-Content -Path C:\Users\usuario\Desktop\listadeemails.txt
+
+#faz laço de repetição e separa os dados com o "@"
+
+foreach($i in $afetados){
+
+    #limpa a informação
+
+    $i = $i -split ('@')
+    $i = $i[0]
+    $i
+
+    #deleta a informação da caixa definida em $i
+
+    Search-Mailbox -Identity "$i" -SearchQuery 'Subject:"1qaz!qaz"' -DeleteContent -force
+
+    #em caso de teste antes de apagar use o sleep
+    #Start-Sleep -Seconds 1
+
+
+}
